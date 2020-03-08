75ccdb181f874a3c73e60648161207ab237cbded
goodmaz1 <45318983+goodmaz1@users.noreply.github.com>
2020-03-08 09:44:37 -0700

    Add files via upload

---
+++ Assets/Node.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Node[] ConnectsTo;
+
+    
+    private void OnDrawGizmos()
+    {
+        //if (!Application.isPlaying) return;
+
+        foreach (Node n in ConnectsTo)
+        {
+            Gizmos.color = Color.red;
+
+            Gizmos.DrawRay(transform.position, (n.transform.position - transform.position).normalized * 4);
+
+            //Gizmos.color = Color.blue;
+            //Gizmos.DrawLine(transform.position, n.transform.position);
+        }
+    }
+}
